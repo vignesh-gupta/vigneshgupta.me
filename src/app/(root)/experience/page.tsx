cb8a0b2aabaf4c68fd78df73bee037b5f43d1372
Vignesh Gupta <vighneshgupta32@gmail.com>
2025-11-30 14:01:43 +0530

    refactor: change import to type for Metadata in experience page

--- src/app/(root)/experience/page.tsx
+++ src/app/(root)/experience/page.tsx
@@ -1,4 +1,4 @@
-import { Metadata } from "next";
+import type { Metadata } from "next";
 
 import PageContainer from "@/components/common/page-container";
 import PageHeader from "@/components/common/page-header";
